587da38289fe67b016037debbebaf39f63e13ac2
Oscar Perez-Hernandez <perezoscar360@gmail.com>
2023-08-18 13:11:52 -0500

    added a square component with the player game board. added the img or x to render depending on the players turn

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import {Square} from './components/Square/Square'
-import './App.css';
+import { Square } from "./components/Square/Square";
+import "./App.css";
+import { useState } from "react";
+import React from 'react'
 
 function App() {
   // ! This is JavaScript
-  const propVariable = 'This is a prop'
+  // const propVariable = 'This is a prop'
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState(true);
 
+  const handleClick = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""]);
+    setPlayer(true);
+  }
 
   return (
-      <Square propVar={propVariable}/>
+    <div className="App">
+      <div className="container">
+        {squares.map((value, index) => {
+          return (
+            <Square
+              squareValue={value}
+              index={index}
+              squares={squares}
+              setSquares={setSquares}
+              player={player}
+              setPlayer={setPlayer}
+            />
+          );
+        })}
+      </div>
+      <button onClick={handleClick}>Reset</button>
+    </div>
   );
 }
 
--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
 export const Square = (props) => {
+    console.log(props.squares, props.player)
+    console.log(props.squareValue)
+
+    //! function to handle when a user clicks on an individual square
+    const handleClick = () => {
+        if(!props.squareValue) {
+            if(props.player) {
+                props.squares.splice(props.index, 1, 'X');
+                props.setSquares(props.squares);
+                props.setPlayer(!props.player);
+            }else {
+                props.squares.splice(props.index, 1, 'O');
+                props.setSquares(props.squares);
+                props.setPlayer(!props.player);
+            }
+        }
+    };
+
   return (
-    <div className='square'>{props.propVariable}</div>
+    <div className='square' onClick={handleClick}>{props.squareValue === "O" ? <img src="https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png" /> : props.squareValue }</div>
   )
 }
